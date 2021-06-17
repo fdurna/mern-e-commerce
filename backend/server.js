b7ae1f360ace63aca6242fb8b6152882494d35dc
fatih durna <fdfatihdurna@gmail.com>
2021-06-17 13:30:32 +0300

    creating redux store , middleware fixed.

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ connectDB()
 
 const app = express()
 
+app.use('/api/products',productRoutes)
+
 app.use(notFound)
 
 app.use(errorHandler)
@@ -18,8 +20,6 @@ app.use(errorHandler)
 app.get('/', (req, res) => {
     res.send('API is running....!!!')
 })
-
-app.use('/api/products',productRoutes)
 
 const PORT = process.env.PORT || 5000
 
