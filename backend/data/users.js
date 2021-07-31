598b7f93cf43c0ccb334ae6b5581b1bfe8a5b94d
fatih durna <fdfatihdurna@gmail.com>
2021-07-31 16:29:53 +0300

    user authentication

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,23 +2,27 @@ import bcrypt from 'bcryptjs'
 
 
 const users = [
-    {
-      name: 'Admin User',
-      email: 'adnin@example.com',
-      password: bcrypt.hashSync('123456',10),
-      isAdmin: true,
-    },
-    {
-        name: 'Yunus Baysal',
-        email: 'yunusbaysal@exapmle.com',
-        password: bcrypt.hashSync('123456',10),
-    },
-    {
-        name: 'Fatih Durna',
-        email: 'fatihdurna@exapmle.com',
-        password: bcrypt.hashSync('123456',10),
-    },
-  ]
-  
-  export default users
-  
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Yunus Baysal',
+    email: 'yunusbaysal@exapmle.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Fatih Durna',
+    email: 'fatihdurna@exapmle.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'John Doe',
+    email: 'john@exapmle.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
