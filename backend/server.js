aa702aa681b3bf1d7fd8ee02c960b0621c312068
fatih durna <fdfatihdurna@gmail.com>
2021-08-01 13:57:58 +0300

    bodyParser added.

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import dotenv  from 'dotenv'
-import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound,errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import bodyParser from 'body-parser';
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Body parser use JSON data
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
