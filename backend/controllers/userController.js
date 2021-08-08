e37bd55a0762f13dfd95cb2863a9cf46126e7f36
fatih durna <fdfatihdurna@gmail.com>
2021-08-08 12:42:34 +0300

    user registration and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,13 +11,13 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            isAdmin:user.isAdmin,
-            token:generateToken(user._id)
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -29,7 +29,7 @@ const authUser = asyncHandler(async (req, res) => {
 // @desc Get user profile
 // @route GET /api/users/login
 // @access Private
-const getUserProfile = asyncHandler(async(req,res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
     if (user) {
         res.json({
@@ -44,6 +44,41 @@ const getUserProfile = asyncHandler(async(req,res) => {
     }
 })
 
-export { authUser , getUserProfile}
+// @desc   Register a new user
+// @route  POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    
+    const userExists = await User.findById({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
+
+export { authUser, registerUser, getUserProfile }
 
 
