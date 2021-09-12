c13a563198d68661f48e4c29681c2935d09666b3
fatih durna <fdfatihdurna@gmail.com>
2021-09-12 16:15:49 +0300

    update profile endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,9 +7,61 @@ import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
+const generateToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'30d',
+    })
+}
+
 router.route('/').post(registerUser)
 router.post('/login', authUser)
 //router.route('/profile').get(protect,getUserProfile)
+router.put('/profile', asyncHandler(async(req,res)=> {
+    let token
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await User.findById(decoded.id).select('-password')
+            const user = await User.findById(req.user._id)
+            if (user) {
+
+                user.name = req.body.name || user.name
+                user.email = req.body.email || user.email
+                
+                if(req.body.password) {
+                    user.password = req.body.password
+                }
+
+                const updateUser = await user.save()
+
+                res.json({
+                    _id: updateUser._id,
+                    name: updateUser.name,
+                    email: updateUser.email,
+                    isAdmin: updateUser.isAdmin,
+                    token: generateToken(updateUser._id)
+                })
+
+            } else {
+                res.status(404)
+                throw new Error('User not found')
+            }
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized,token failed')
+        }
+    }
+    if (!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
+}))
+
 router.get('/profile', asyncHandler(async (req, res)=> {
     let token
     if (
