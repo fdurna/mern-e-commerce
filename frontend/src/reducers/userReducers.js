e1c805034cab4c509e4574fc65bfbfc1027aaa53
fatih durna <fdfatihdurna@gmail.com>
2021-09-14 23:49:48 +0300

    user authentication and profile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
 } from '../constants/userConstants'
 
 
@@ -45,6 +48,29 @@ export const userRegisterReducer = (state={},action) => {
                 userInfo:action.payload
             }
         case USER_REGISTER_FAIL:
+            return {
+                loading:false,
+                error:action.payload
+            }
+        case USER_LOGOUT:
+            return {}
+        default:
+            return state;
+    }    
+}
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return {
+                loading:true,
+            }
+        case USER_DETAILS_SUCCESS:
+            return {
+                loading:false,
+                user:action.payload
+            }
+        case USER_DETAILS_FAIL:
             return {
                 loading:false,
                 error:action.payload
